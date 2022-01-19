7a4c8e370a35f425d93218dbfe3eb1e37573144c
coolAryan <42404394+coolAryan@users.noreply.github.com>
2022-01-19 11:53:38 +0530

    Time: 417 ms (8.15%), Space: 13.8 MB (50.59%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        v=len(isConnected)
+        par=[i for i in range(v)]
+        print(par)
+        count=v
+        for i in range(v):
+            for j in range(i+1,v):
+                if isConnected[i][j]==1:
+                    print(i,j)
+                    pi,pj=i,j
+                    while par[pi]!=pi:
+                        pi=par[pi]
+                    while par[pj]!=pj:
+                        pj=par[pj]
+                    if pi!=pj:
+                        count-=1
+                        par[pi]=pj
+        return count
+        
