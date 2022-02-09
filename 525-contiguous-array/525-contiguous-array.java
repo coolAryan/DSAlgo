95eb49a6bd962b94d9ecb929c0ca54b369d4bfc2
coolAryan <42404394+coolAryan@users.noreply.github.com>
2022-02-09 10:38:14 +0530

    Time: 45 ms (32.43%), Space: 90.5 MB (13.14%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -3,19 +3,16 @@ class Solution {
         Map<Integer,Integer>m=new HashMap<>();
         int lon=0;
         int sum=0;
+        m.put(0,-1);//for making sure in case we encounter 0,since we dont have 0 yet so -1 is used                     // as its value
         for(int i=0;i<nums.length;i++)
         {
             
-                sum+=nums[i]==0?-1:1;
-            
-          
-            if(sum==0)
-            {
-                if(lon<i+1)
-                {
-                 lon=i+1;
-                }
-            }
+                sum+=nums[i]==0?-1:1;  
+           // if we encounter sum as 0 it means we have equal no of 0 and 1 ,
+          // from the start till curr index. so =>i+1
+           
+            //if we encounter same sum value again ,it means equal 0 and 1,
+            //between same sum value that had occured previously and now so=>i-m.get(sum)
             if(m.containsKey(sum))
             {
                 
