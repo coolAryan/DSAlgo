57498e4efb8292aa9f014b6ad0c7b2470362e03d
coolAryan <42404394+coolAryan@users.noreply.github.com>
2022-02-16 00:42:45 +0530

    Time: 8 ms (49.44%), Space: 45.8 MB (36.23%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int a[]=new int[128];
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            char c=ransomNote.charAt(i);
+            a[c-'a']++;
+        }
+         for(int i=0;i<magazine.length();i++)
+        {
+             char c=magazine.charAt(i);
+             a[c-'a']--;
+        }
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            char c=ransomNote.charAt(i);
+            if(a[c-'a']>0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
